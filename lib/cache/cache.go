09b47394187693424a7618675ac3d59113453ace
Andrea Di Persio <andrea.dipersio@gmail.com>
2016-04-26 01:10:25 +0200

    gofmt

--- lib/cache/cache.go
+++ lib/cache/cache.go
@@ -1,49 +1,47 @@
 package cache
 
 import (
-    "log"
+	"log"
 )
 
 type CacheRequest struct {
-    Filename, Content string
+	Filename, Content string
 }
 
 type Cache struct {
-    Data     map[string] string
+	Data map[string]string
 
-    GetChan  chan string
-    PutChan  chan CacheRequest
+	GetChan chan string
+	PutChan chan CacheRequest
 }
 
 func NewCache() *Cache {
-    return &Cache {
-        Data    : make(map[string] string, 10),
-        GetChan : make(chan string),
-        PutChan : make(chan CacheRequest),
-    }
+	return &Cache{
+		Data:    make(map[string]string, 10),
+		GetChan: make(chan string),
+		PutChan: make(chan CacheRequest),
+	}
 }
 
 func (c *Cache) Run() {
-    log.Println("Starting cache")
+	log.Println("Starting cache")
 
-    for {
-        select {
+	for {
+		select {
 
-        case filename := <-c.GetChan:
-            log.Printf("cache::get %v", filename)
-            content := c.Data[filename]
+		case filename := <-c.GetChan:
+			log.Printf("cache::get %v", filename)
+			content := c.Data[filename]
 
-            if content == "" {
-                log.Print("cache::miss")
-            }
+			if content == "" {
+				log.Print("cache::miss")
+			}
 
-            c.GetChan <-content
+			c.GetChan <- content
 
-        case put := <-c.PutChan:
-            log.Printf("cache::put %v", put.Filename)
-            c.Data[put.Filename] = put.Content
-        }
-    }
+		case put := <-c.PutChan:
+			log.Printf("cache::put %v", put.Filename)
+			c.Data[put.Filename] = put.Content
+		}
+	}
 }
-
-
--- main.go
+++ main.go
@@ -1,217 +1,215 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "flag"
+	"flag"
 
-    "bytes"
-    "strings"
+	"bytes"
+	"strings"
 
-    "os/exec"
+	"os/exec"
 
-    "net/http"
+	"net/http"
 
-    "encoding/json"
-    "encoding/base64"
+	"encoding/base64"
+	"encoding/json"
 
-    "html/template"
+	"html/template"
 
-    "strconv"
+	"strconv"
 
-    "github.com/andreadipersio/pugo/lib/cache"
+	"github.com/andreadipersio/pugo/lib/cache"
 )
 
 const (
-    githubContentURL = "https://api.github.com/repos/%v/%v/contents/%v"
+	githubContentURL = "https://api.github.com/repos/%v/%v/contents/%v"
 )
 
 var (
-    httpPort int
+	httpPort int
 
-    repo     string
-    owner    string
-    token    string
+	repo  string
+	owner string
+	token string
 
-    staticsPath  string
-    templatesPath string
-
+	staticsPath   string
+	templatesPath string
 )
 
 type GithubResponse struct {
-    Content string `json:"content"`
+	Content string `json:"content"`
 }
 
 func getTemplatePath(filename string) string {
-    return fmt.Sprintf("%v/%v", templatesPath, filename)
+	return fmt.Sprintf("%v/%v", templatesPath, filename)
 }
 
 func init() {
-    flag.IntVar(&httpPort, "port", 8002, "HTTP Server port")
+	flag.IntVar(&httpPort, "port", 8002, "HTTP Server port")
 
-    flag.StringVar(&repo, "repo", "", "Github public repository name")
-    flag.StringVar(&owner, "owner", "", "Github public repository owner")
-    flag.StringVar(&staticsPath, "statics", "./statics", "path to static files")
-    flag.StringVar(&templatesPath, "templates", "./templates", "path to html templates")
-    flag.StringVar(&token, "token", "", "Github Personal Token")
+	flag.StringVar(&repo, "repo", "", "Github public repository name")
+	flag.StringVar(&owner, "owner", "", "Github public repository owner")
+	flag.StringVar(&staticsPath, "statics", "./statics", "path to static files")
+	flag.StringVar(&templatesPath, "templates", "./templates", "path to html templates")
+	flag.StringVar(&token, "token", "", "Github Personal Token")
 
-    flag.Parse()
+	flag.Parse()
 }
 
 func getFilename(urlPath string) string {
-    parts := strings.Split(urlPath, "/")
-    filename := parts[len(parts) - 1]
+	parts := strings.Split(urlPath, "/")
+	filename := parts[len(parts)-1]
 
-    if filename == "" {
-        return "README.md"
-    }
+	if filename == "" {
+		return "README.md"
+	}
 
-    return fmt.Sprintf("%v.md", filename)
+	return fmt.Sprintf("%v.md", filename)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request, localCache *cache.Cache) {
-    var content string
+	var content string
 
-    filename := getFilename(r.URL.Path)
+	filename := getFilename(r.URL.Path)
 
-    log.Printf("[REQ] %v", filename)
+	log.Printf("[REQ] %v", filename)
 
-    refreshFlag := r.FormValue("refresh")
+	refreshFlag := r.FormValue("refresh")
 
-    getFromCache := func() string {
-        localCache.GetChan <- filename
-        return <-localCache.GetChan
-    }
+	getFromCache := func() string {
+		localCache.GetChan <- filename
+		return <-localCache.GetChan
+	}
 
-    getFromGithub := func() (content string) {
-        githubResp := &GithubResponse{}
+	getFromGithub := func() (content string) {
+		githubResp := &GithubResponse{}
 
-        fetch := func() error {
-            remoteURL := fmt.Sprintf(githubContentURL, owner, repo, filename)
+		fetch := func() error {
+			remoteURL := fmt.Sprintf(githubContentURL, owner, repo, filename)
 
-            log.Println(remoteURL)
+			log.Println(remoteURL)
 
-            client := http.Client{}
-            req, err := http.NewRequest("GET", remoteURL, nil)
+			client := http.Client{}
+			req, err := http.NewRequest("GET", remoteURL, nil)
 
-            r.Header.Set("User-Agent", "andreadipersio/pugo")
+			r.Header.Set("User-Agent", "andreadipersio/pugo")
 
-            if token != "" {
-                r.SetBasicAuth(owner, token)
-            }
+			if token != "" {
+				r.SetBasicAuth(owner, token)
+			}
 
-            if err != nil {
-                panic(err)
-            }
+			if err != nil {
+				panic(err)
+			}
 
-            resp, err := client.Do(req)
+			resp, err := client.Do(req)
 
-            if err != nil {
-                panic(err)
-            }
+			if err != nil {
+				panic(err)
+			}
 
-            defer resp.Body.Close()
+			defer resp.Body.Close()
 
-            dec := json.NewDecoder(resp.Body)
+			dec := json.NewDecoder(resp.Body)
 
-            if err := dec.Decode(githubResp); err != nil {
-                return err
-            }
+			if err := dec.Decode(githubResp); err != nil {
+				return err
+			}
 
-            return nil
-        }
+			return nil
+		}
 
-        decode := func() error {
-            b64 := base64.StdEncoding
+		decode := func() error {
+			b64 := base64.StdEncoding
 
-            if v, err := b64.DecodeString(githubResp.Content); err != nil {
-                return err
-            } else {
-                content = string(v)
-            }
+			if v, err := b64.DecodeString(githubResp.Content); err != nil {
+				return err
+			} else {
+				content = string(v)
+			}
 
-            return nil
-        }
+			return nil
+		}
 
-        toHTML := func() error {
-            cmd := exec.Command("markdown")
+		toHTML := func() error {
+			cmd := exec.Command("markdown")
 
-            if pipeWriter, err := cmd.StdinPipe(); err != nil {
-                return err
-            } else {
-                pipeWriter.Write([]byte(content))
-                pipeWriter.Close()
-            }
+			if pipeWriter, err := cmd.StdinPipe(); err != nil {
+				return err
+			} else {
+				pipeWriter.Write([]byte(content))
+				pipeWriter.Close()
+			}
 
-            if output, err := cmd.Output(); err != nil {
-                return err
-            } else {
-                content = string(output)
-            }
+			if output, err := cmd.Output(); err != nil {
+				return err
+			} else {
+				content = string(output)
+			}
 
-            if tmpl, err := template.ParseFiles(getTemplatePath("base.html"), getTemplatePath("article.html")); err != nil {
-                return err
-            } else {
-                context := &struct { Content template.HTML } { template.HTML(content), }
+			if tmpl, err := template.ParseFiles(getTemplatePath("base.html"), getTemplatePath("article.html")); err != nil {
+				return err
+			} else {
+				context := &struct{ Content template.HTML }{template.HTML(content)}
 
-                wr := bytes.NewBufferString("")
+				wr := bytes.NewBufferString("")
 
-                if parseErr := tmpl.Execute(wr, context); parseErr != nil {
-                    return parseErr
-                }
+				if parseErr := tmpl.Execute(wr, context); parseErr != nil {
+					return parseErr
+				}
 
-                content = wr.String()
-            }
+				content = wr.String()
+			}
 
-            localCache.PutChan <-cache.CacheRequest{filename, content}
+			localCache.PutChan <- cache.CacheRequest{filename, content}
 
-            return nil
-        }
+			return nil
+		}
 
-        if err := fetch(); err != nil {
-            log.Panicf("Cannot get data from github: %v", err)
-        }
+		if err := fetch(); err != nil {
+			log.Panicf("Cannot get data from github: %v", err)
+		}
 
-        if err := decode(); err != nil {
-            log.Panicf("Cannot decode github response: %v", err)
-        }
+		if err := decode(); err != nil {
+			log.Panicf("Cannot decode github response: %v", err)
+		}
 
-        if err := toHTML(); err != nil {
-            log.Panicf("Cannot convert markdown to html: %v", err)
-        }
+		if err := toHTML(); err != nil {
+			log.Panicf("Cannot convert markdown to html: %v", err)
+		}
 
-        log.Println("[Github] OK")
+		log.Println("[Github] OK")
 
-        return
-    }
+		return
+	}
 
-    if content = getFromCache(); content == "" || refreshFlag != "" {
-        content = getFromGithub()
-    }
+	if content = getFromCache(); content == "" || refreshFlag != "" {
+		content = getFromGithub()
+	}
 
-    w.Header().Set("Content-Type", "text/html")
-    w.Header().Set("Content-Length", strconv.Itoa(len(content)))
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Length", strconv.Itoa(len(content)))
 
-    fmt.Fprint(w, content)
+	fmt.Fprint(w, content)
 }
 
 func main() {
-    httpAddr := fmt.Sprintf(":%v", httpPort)
+	httpAddr := fmt.Sprintf(":%v", httpPort)
 
-    log.Printf("Serving static files from %v", staticsPath)
+	log.Printf("Serving static files from %v", staticsPath)
+	log.Printf("Starting server on %v", httpAddr)
 
-    log.Printf("Starting server on %v", httpAddr)
+	localCache := cache.NewCache()
+	go localCache.Run()
 
-    localCache := cache.NewCache()
-    go localCache.Run()
+	http.HandleFunc("/favicon.ico", http.NotFound)
+	http.Handle("/s/", http.StripPrefix("/s/", http.FileServer(http.Dir(staticsPath))))
 
-    http.HandleFunc("/favicon.ico", http.NotFound)
-    http.Handle("/s/", http.StripPrefix("/s/", http.FileServer(http.Dir(staticsPath))))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rootHandler(w, r, localCache)
+	})
 
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        rootHandler(w, r, localCache)
-    })
-
-    log.Fatal(http.ListenAndServe(httpAddr, nil))
+	log.Fatal(http.ListenAndServe(httpAddr, nil))
 }
